537f609f9864fe42e10a016e6c507883d7ee750a
harshy23 <0harshmishra0@gmail.com>
2025-05-26 19:58:47 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Minimalui from './minimalui'
+import Minimalui from './Minimalui'
 
 function App() {
   const [count, setCount] = useState(0)
